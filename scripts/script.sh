b4e41619ed547932cd029f1427c8854002a33693
stsirtsis <stsirtsis@mpi-sws.org>
2025-02-01 17:05:23 +0100

    minor edit in bash script

--- scripts/script.sh
+++ scripts/script.sh
@@ -3,10 +3,10 @@
 source env/bin/activate
 
 exp_name="test"
-N=10
+# N=10
 iterations=10
-prob=0.5
-seed=42
+# prob=0.5
+# seed=42
 output_dir="outputs"
 
 # individual run (python)
@@ -20,13 +20,12 @@ output_dir="outputs"
 N_seq=(10 20)
 prob_seq=(0.1 0.5)
 seed_seq=(1 2 3 4 5 6 7 8 9 10)
-for N in "${N_seq[@]}"
-do
-    for prob in "${prob_seq[@]}"
-    do
-        for seed in "${seed_seq[@]}"
-        do
+for N in "${N_seq[@]}"; do
+    for prob in "${prob_seq[@]}"; do
+        for seed in "${seed_seq[@]}"; do
             python src/experiment.py --exp_name=$exp_name --N=$N --iterations=$iterations --prob=$prob --seed=$seed --output_dir=$output_dir
         done
     done
 done
+
+deactivate
