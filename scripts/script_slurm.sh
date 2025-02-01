3d0e01dd6fc725ff9de1982f07e75873a9aecec0
stsirtsis <stsirtsis@mpi-sws.org>
2025-02-01 17:42:49 +0100

    print parameters in slurm job output

--- scripts/script_slurm.sh
+++ scripts/script_slurm.sh
@@ -7,10 +7,17 @@
 #SBATCH -o outputs/slurm_logs/simulation_%j.out      # File to which STDOUT will be written
 #SBATCH -e outputs/slurm_logs/simulation_%j.err      # File to which STDERR will be written
 
+# print the parameters of the parent script (N, prob, seed)
+echo "Job $SLURM_JOB_ID with parameters:"
+echo "- N: $N"
+echo "- prob: $prob"
+echo "- seed: $seed"
+echo ""
+
 source env/bin/activate
 
 # run python script
-python -m src.experiment --output=outputs/experiment_N_${N} --n=$N --seeds=$seeds --njobs=$njobs
+python src/experiment.py --exp_name=$exp_name --N=$N --iterations=$iterations --prob=$prob --seed=$seed --output_dir=$output_dir
 
 # increase N by 1
 N=$((N+1))
