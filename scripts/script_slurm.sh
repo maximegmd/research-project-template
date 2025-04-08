4175939974ecf0c731dce12aefcf74b1b1d42208
stsirtsis <stsirtsis@mpi-sws.org>
2025-04-08 10:02:35 +0200

    fix typo in logs

--- scripts/script_slurm.sh
+++ scripts/script_slurm.sh
@@ -4,8 +4,8 @@
 #SBATCH -N 1                    # Ensure that all cores are on one machine
 #SBATCH -t 0-02:59              # Maximum run-time in D-HH:MM
 #SBATCH --mem=5G               # Memory pool for all cores (see also --mem-per-cpu)
-#SBATCH -o outputs/slurm_logs/simulation_%A_$a.out      # File to which STDOUT will be written
-#SBATCH -e outputs/slurm_logs/simulation_%A_$a.err      # File to which STDERR will be written
+#SBATCH -o outputs/slurm_logs/simulation_%A_%a.out      # File to which STDOUT will be written
+#SBATCH -e outputs/slurm_logs/simulation_%A_%a.err      # File to which STDERR will be written
 
 source env/bin/activate
 
